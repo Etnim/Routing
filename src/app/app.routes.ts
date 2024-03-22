e516a4896db89586c30a7c1a0a6cf83d0f3c6a57
Anhelina Velychko <avelichko469@gmail.com>
2024-03-22 13:55:40 +0200

    Added not found to routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import {ContactsComponent} from "./contacts/contacts.component";
+import {FAQComponent} from "./faq/faq.component";
+import {NotFoundComponent} from "./not-found/not-found.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path :'contacts',
+    component : ContactsComponent,
+  },
+  {
+    path :'faq',
+    component: FAQComponent,
+  },
+  {
+    path:'**',
+    component: NotFoundComponent,
+  }
+];
